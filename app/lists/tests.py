7ef1b00a91a46d53829a3fee68316718314d27f8
eg_acorn <rasakjustone01.11@gmail.com>
2022-12-28 18:40:53 +0300

    Wrote a simple module test

---
+++ app/lists/tests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
+
+
+# Create your tests here.
+class SmokeTest(TestCase):
+    '''Тест домашней страницы'''
+    
+    def test_root_url_resolves_to_home_page_view(self):
+        '''
+        Тест: корневой url преобразуется в представление
+        домашней страницы
+        '''
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
