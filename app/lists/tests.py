74ca169b845bb1db0dfa321c1f4bc01f7194b5c1
eg_acorn <rasakjustone01.11@gmail.com>
2022-12-30 00:07:40 +0300

    Updated lists/tests.py

--- app/lists/tests.py
+++ app/lists/tests.py
@@ -1,16 +1,28 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
 from lists.views import home_page
 
 
 # Create your tests here.
-class SmokeTest(TestCase):
-    '''Тест домашней страницы'''
+class HomePageTest(TestCase):
+    """Тест домашней страницы"""
     
     def test_root_url_resolves_to_home_page_view(self):
-        '''
+        """
         Тест: корневой url преобразуется в представление
         домашней страницы
-        '''
-        found = resolve('/')
+        """
+        found = resolve("/")
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode("utf8")
+        # self.assertTrue(html.startswith("<!DOCTYPE html>"))
+        # self.assertTrue(html.startswith("<html>"))
+        # Протестируем, что в html на самом деле
+        self.assertIn("<kek>", html)
+        self.assertIn("<title>To-Do lists</title>", html)
+        self.assertTrue(html.endswith("</html>"))
